32e12e6c657dcd22d5839974252a59c55b92c0e0
Joel W. King <joel.king@wwt.com>
2020-07-16 14:35:16 -0400

    added "app" module, tested query, delete

---
+++ plugins/module_utils/Pensando.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+#
+#     Copyright (c) 2020 World Wide Technology, LLC
+#     All rights reserved.
+
+import time
+import json
+import requests
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+
+class Pensando(object):
+    """
+        Class to manage the connection with the Pensando Policy and Service Manager (PSM)
+        In documentation, you may see this referred to by the codename 'Venice'
+    """
+    def __init__(self, api_version=None, password=None, username=None, hostname=None, rate_limit_retry=4):
+        """
+        """
+        self.rate_limit_retry = rate_limit_retry   # Number of attempts to issue the command before assuming the 429 is persistent
+
+        self.api_version = api_version
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        #
+        self.changed = False
+        self.cookie = None
+        self.headers = {'content-type': 'application/json'}
+        self.verify = False
+
+    def login(self, tenant='default'):
+        """
+            To access and execute API calls against a Venice cluster, the client must first authenticate itself.
+            This done via the login API endpoint. Once the client has made a successful login, it will
+            receive a cookie (Session ID) in the header “Set-Cookie” of the response from the login call.
+            The header contains the cookie name, “sid”, value, expiry time, and other info.
+
+            The cookie is used to authenticate each API call made to Venice. The client will need to provide
+            the cookie in the header in all subsequent requests to Venice.
+        """
+
+        payload = json.dumps(dict(username=self.username, password=self.password, tenant=tenant))
+
+        r = requests.request('POST', 'https://{}/{}/login'.format(self.hostname, self.api_version), headers=self.headers, data=payload, verify=self.verify)
+
+        if r.ok:
+            self.cookie = dict(sid=r.cookies.get('sid'))
+
+        return r
+
+    def rate_limit(self, verb, url, **kwargs):
+        """
+            Policy from ADM runs can be large, provide logic to handle rate limit errors
+            As this method handles all API calls, it also provides a single point to swap out 'requests'.
+        """
+        url = 'https://{}' + url
+        url = url.format(self.hostname, self.api_version)
+
+        for _ in range(self.rate_limit_retry):
+            r = requests.request(verb, url, verify=self.verify, cookies=self.cookie, **kwargs)
+            if r.status_code == requests.codes.TOO_MANY_REQUESTS:
+                time.sleep(int(r.headers.get("Retry-After", 1)))    # TODO, verify, this is from Meraki
+            else:
+                return r
+        return r
+
+    def manage_policy(self, params):
+        """
+            Logic to apply or update an existing policy
+            We expect 'rules' to be a list of dictionaries with the correct keywork and values
+
+            TODO this method is only prelimiary code !!!
+        """
+
+        url = '/configs/security/{}/networksecuritypolicies'
+
+        payload = json.dumps({"kind": "NetworkSecurityPolicy",
+                              "api-version": params.get('api_version'),
+                              "meta": {"name": params.get('policy_name'),
+                                       "tenant": params.get('tenant'),
+                                       "namespace": params.get('namespace')
+                                      },
+                              "spec": {"attach-tenant": params.get('attach_tenant'),
+                                       "rules": params.get('rules')
+                                      }
+                              }
+                            )
+
+        policy = self.rate_limit('POST', url, data=payload)
+
+        if policy.status_code == requests.codes.conflict:                   # already exists!
+            self.changed = False
+            # TODO Query the existing policy?
+        else:
+            self.changed = True
+
+        return policy
+
+    def manage_app(self, params):
+        """
+            Logic to apply or update an App
+
+            TODO this method is only prelimiary code !!!
+        """
+
+        url = '/configs/security/{}/apps'
+
+        payload = {"kind": "App",
+                   "api-version": params.get('api_version'),
+                   "meta": {"name": params.get('app_name'),
+                            "tenant": params.get('tenant'),
+                            "namespace": params.get('namespace')
+                           },
+                    "spec": {}
+                  }
+
+        if params.get('alg'):
+            payload['spec']['alg'] = params.get('alg')
+        if params.get('proto_ports'):
+            payload['spec']['proto-ports'] = params.get('proto_ports')
+
+        # Verify the user specified 'alg' or 'proto_ports', (both is also acceptable)
+        if len(payload['spec'] ) == 0:
+            # TODO Fail!
+            return None
+
+
+        payload = json.dumps(payload)
+        app = self.rate_limit('POST', url, data=payload)
+
+        if app.status_code == requests.codes.bad_request:                # payloade is incorrect
+            self.changed = False
+        if app.status_code == requests.codes.conflict:                   # already exists!
+            self.changed = False
+            # TODO Query the existing policy?
+        else:
+            self.changed = True
+
+        return app
--- plugins/modules/app.py
+++ plugins/modules/app.py
@@ -5,17 +5,15 @@
 
 DOCUMENTATION = '''
 ---
-module: network_security_policy
+module: app
 
-short_description: Create, update, query or delete a Network Security Policy
+short_description: Create, update, query or delete an App
 
 version_added: "2.9"
 
 description:
-    - A Network Security Policy contains firewall rules such as 'to' and 'from', 'ports', 'protocols, etc.
-    - and is applied to the Pensando policy and services manager (PSM). These policies are propagated to
-    - the Distributed Service Card (DSC) by the PSM. This module programatically manages the policy using the
-    - API of the PSM.
+    - An App is a network service defined either by a "protocol, port" pair, or by an application level 
+    - gateway (i.e. "ALG"). This module programatically manages the policy using the API of the PSM.
 
 options:
     tenant:
@@ -30,21 +28,19 @@ options:
         required: false
         default: 'default'
 
-    policy_name:
+    app_name:
         description:
-            - Name of the network security policy (only one network security policy is currently allowed)
-        required: false
-        default: 'default'
+            - Name of the App
+        required: True
 
-    attach_tenant:
+    alg:
         description:
-            - A Network Security Policy rule is typically deployed tenant-wide. Specify True to enable.
+            - Application level gateway specification
         required: false
-        default: true
 
-    rules:
+    proto_ports:
         description:
-            - A list of dictionary objects which define the firewall rules to be applied to the PSM
+            - List of protocol, port pairs
         required: false
 
     state:
@@ -83,39 +79,7 @@ author:
 
 EXAMPLES = '''
 
-- network_security_policy:
-      hostname: psm.example.net
-      username: admin
-      password: '{{ password }}'
-      api_version: v1
-      tenant: default
-      namespace: default
-      state: present
-      policy_name: quarantine
-      rules:
-        - action: deny
-          from-ip-addresses:
-            - 198.51.100.0/24
-          proto-ports:
-            - ports: '123'
-              protocol: udp
-          to-ip-addresses:
-            - 192.0.2.0/24
 
-- name: Query Policy
-  network_security_policy:
-      hostname: psm.example.net
-      username: admin
-      password: '{{ password }}'
-      state: query
-
-- name: Delete Policy
-  network_security_policy:
-      hostname: psm.example.net
-      username: admin
-      password: '{{ password }}'
-      state: absent
-      policy_name: foo
 
 
 '''
@@ -145,12 +109,11 @@ def main():
             password=dict(required=True, no_log=True),
             state=dict(required=False, default='present'),
             tenant=dict(required=False, default='default'),
-            rules=dict(required=False, type='list'),
-            attach_tenant=dict(required=False, default=True, type='bool'),
-            policy_name=dict(required=False, default='default'),
+            alg=dict(required=False, type='list', default=[]),
+            proto_ports=dict(required=False, type='list', default=[]),
+            app_name=dict(required=False, default='default'),
             namespace=dict(required=False, default='default')
             ),
-            check_invalid_arguments=True,
             add_file_common_args=True,
             supports_check_mode=False
         )
@@ -166,7 +129,7 @@ def main():
         module.fail_json(msg='{}:{}'.format(login.status_code, login.text))
 
     if module.params.get('state') == 'query':
-        url = '/configs/security/{}/networksecuritypolicies'
+        url = '/configs/security/{}/apps'
         policy = psm.rate_limit('GET', url)
         if policy.ok:
             module.exit_json(changed=False, policy=policy.json())
@@ -174,7 +137,7 @@ def main():
             module.fail_json(msg='{}:{}'.format(policy.status_code, policy.text))
 
     elif module.params.get('state') == 'absent':
-        url = '/configs/security/{}/networksecuritypolicies/{}'.format('{}', module.params.get('policy_name'))
+        url = '/configs/security/{}/apps/{}'.format('{}', module.params.get('app_name'))
         policy = psm.rate_limit('DELETE', url)
         if policy.status_code == requests.codes.NOT_FOUND:
             module.exit_json(changed=False, policy=policy.json())
@@ -182,7 +145,7 @@ def main():
             module.exit_json(changed=True, policy=policy.json())
 
     elif module.params.get('state') == 'present':
-        policy = psm.manage_policy(module.params)
+        policy = psm.manage_app(module.params)
         if policy.ok:
             module.exit_json(changed=psm.changed, policy=policy.json())
         else:
